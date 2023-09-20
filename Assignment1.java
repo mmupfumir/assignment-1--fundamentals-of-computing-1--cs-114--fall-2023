f8d409d6cbec39f97445b360f86a504e3c81f503
mmupfumir <mmupfumir@hartford.edu>
2023-09-20 02:08:58 -0400

    moved scanner close to bottom of program

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,58 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+
+        System.out.println("  MM          MM      MM          MM");
+        System.out.println("  MM MM    MM MM      MM MM    MM MM");
+        System.out.println("  MM  MM  MM  MM      MM  MM  MM  MM");
+        System.out.println("  MM    MM    MM      MM    MM    MM");
+        System.out.println("  MM          MM      MM          MM");
+        System.out.println("  MM          MM      MM          MM");
+        System.out.println("  MM          MM      MM          MM");
+        System.out.println("  MM          MM      MM          MM");
+    //create scanner
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Please enter a temperature in Farenheit: ");
+    
+      double Farenheit = scanner.nextDouble();
+      
+      scanner.close();
+    //convert farenheit to celsius
+    double celsius = (Farenheit - 32) * 5.0/9.0;
+    
+    System.out.println(celsius);
+
+    //prompt user for 5 character string
+    System.out.print("Please enter a 5-character string: ");
+
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        if (input.length() == 5) 
+        {
+          //reverse 5 character string
+          String reversedString = reverseString(input);
+          //print reversed string
+          System.out.println(reversedString);
+        } 
+        else 
+        {
+            System.out.println("Invalid input... Please enter exactly 5 characters");
+        }
+    //made function to reverse string
+    public static String reverseString(String input) {
+      StringBuilder reversed = new StringBuilder(input).reverse();
+
+      //convert StrignBuilder back to a string
+      return reversed.toString();
+    }
+  }
+
+  private static String reverseString(String input) {
+    return null;
   }
 }
