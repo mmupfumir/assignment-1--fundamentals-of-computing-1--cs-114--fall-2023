e277c029a9ff6c9b13dbff538e17d07146e4a6c8
mmupfumir <mmupfumir@hartford.edu>
2023-09-20 16:47:47 -0400

    removed else statement and cleaned up code

--- Assignment1.java
+++ Assignment1.java
@@ -1,10 +1,9 @@
-// Import libraries here
 import java.util.Random;
 import java.util.Scanner;
 
 public class Assignment1 {
-  public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        
         System.out.println("  MM          MM      MM          MM");
         System.out.println("  MM MM    MM MM      MM MM    MM MM");
         System.out.println("  MM  MM  MM  MM      MM  MM  MM  MM");
@@ -13,37 +12,33 @@ public class Assignment1 {
         System.out.println("  MM          MM      MM          MM");
         System.out.println("  MM          MM      MM          MM");
         System.out.println("  MM          MM      MM          MM");
-    //create scanner
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.print("Please enter a temperature in Farenheit: ");
-    double Farenheit = scanner.nextDouble();
-    //consumes any new line characters
-    scanner.nextLine();
-    //convert farenheit to celsius
-    double celsius = (Farenheit - 32) * 5.0/9.0;
 
-    //prompt user for 5 character string
-    System.out.print("Please enter a 5-character string: ");
-      String input = scanner.nextLine();
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a temperature in Fahrenheit: ");
+        double Fahrenheit = scanner.nextDouble();
+        //consume any new line characters
+        scanner.nextLine();
+        // Convert Fahrenheit to Celsius
+        double celsius = (Fahrenheit - 32) * 5.0/9.0;
 
-      StringBuilder reversed = new StringBuilder(input).reverse();
+        // prompt user for a 5-character string
+        System.out.print("Enter a 5-character string: ");
+        String input = scanner.nextLine();
 
-        if (input.length() >= 3) 
-        {
-          String reversedString;
-          input = reversedString.substring(1, input.length() - 1);
-        } 
-        else 
-        {
-            System.out.println("Invalid input... Please enter exactly 5 characters");
-        }
+        // reverse string and print drop front and back letters if input is greater than or equal to 5
+        StringBuilder reversedString = new StringBuilder(input).reverse();
+        if (input.length() >= 3) {
+            input = reversedString.substring(1, input.length() - 1);
 
-      Random random = new Random();
+        // Generate a random number between 32 and 16,384
+        Random random = new Random();
         int randomNumber = random.nextInt(16353) + 32;
 
-        // Combined the results and print them to complete the program
+        // Combine the results and print them
         System.out.println("Result: " + celsius + input + randomNumber);
-  scanner.close();
-  }
+        
+        scanner.close();
+    }
 }
